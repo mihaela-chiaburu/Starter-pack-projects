abd682037c248cdcb351a562fe5455ab1a769616
Mihaela C <mchiaburu.1337@gmail.com>
2025-07-15 02:15:06 +0300

    dockerfile

--- StarterPack/Program.cs
+++ StarterPack/Program.cs
@@ -6,6 +6,9 @@ using StarterPack.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
