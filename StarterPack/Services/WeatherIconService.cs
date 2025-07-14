d2276ae9098472f125bba61098f83eec4395ec7a
Mihaela C <mchiaburu.1337@gmail.com>
2025-07-15 02:51:48 +0300

    paths

--- StarterPack/Services/WeatherIconService.cs
+++ StarterPack/Services/WeatherIconService.cs
@@ -7,27 +7,27 @@ namespace StarterPack.Services
     {
         private readonly Dictionary<string, string> _conditionIcons = new()
         {
-            { "sunny", "~/images/weather-icons/Sunny.png" },
-            { "clear", "~/images/weather-icons/clear-night.png" },
-            { "overcast", "~/images/weather-icons/Overcast.png" },
-            { "mist", "~/images/weather-icons/mist.png" },
-            { "sleet", "~/images/weather-icons/sleet.png" },
-            { "freezing drizzle", "~/images/weather-icons/freezing drizzle.png" },
-            { "thunder", "~/images/weather-icons/Thunder.png" },
-            { "blizzard", "~/images/weather-icons/blizzard.png" },
-            { "light drizzle", "~/images/weather-icons/light drizzle.png" },
-            { "ice pellets", "~/images/weather-icons/ice-pellets.png" },
-            { "cloudy", "~/images/weather-icons/cloudy.png" },
-            { "blowing snow", "~/images/weather-icons/Blowing snow.png" },
-            { "heavy snow", "~/images/weather-icons/heavy snow.png" },
-            { "snow", "~/images/weather-icons/snow.png" },
-            { "freezing fog", "~/images/weather-icons/freezing fog.png" },
-            { "fog", "~/images/weather-icons/fog.png" },
-            { "heavy rain", "~/images/weather-icons/heavy rain.png" },
-            { "rain shower", "~/images/weather-icons/heavy rain.png" },
-            { "freezing rain", "~/images/weather-icons/freezing drizzle.png" },
-            { "rain with thunder", "~/images/weather-icons/thunder.png" },
-            { "rain", "~/images/weather-icons/rain.png" }
+            { "sunny", "images/weather-icons/Sunny.png" },
+            { "clear", "images/weather-icons/clear-night.png" },
+            { "overcast", "images/weather-icons/Overcast.png" },
+            { "mist", "images/weather-icons/mist.png" },
+            { "sleet", "images/weather-icons/sleet.png" },
+            { "freezing drizzle", "images/weather-icons/freezing drizzle.png" },
+            { "thunder", "images/weather-icons/Thunder.png" },
+            { "blizzard", "images/weather-icons/blizzard.png" },
+            { "light drizzle", "images/weather-icons/light drizzle.png" },
+            { "ice pellets", "images/weather-icons/ice-pellets.png" },
+            { "cloudy", "images/weather-icons/cloudy.png" },
+            { "blowing snow", "images/weather-icons/Blowing snow.png" },
+            { "heavy snow", "images/weather-icons/heavy snow.png" },
+            { "snow", "images/weather-icons/snow.png" },
+            { "freezing fog", "images/weather-icons/freezing fog.png" },
+            { "fog", "images/weather-icons/fog.png" },
+            { "heavy rain", "images/weather-icons/heavy rain.png" },
+            { "rain shower", "images/weather-icons/heavy rain.png" },
+            { "freezing rain", "images/weather-icons/freezing drizzle.png" },
+            { "rain with thunder", "images/weather-icons/thunder.png" },
+            { "rain", "images/weather-icons/rain.png" }
         };
 
         public string GetWeatherIcon(string condition, bool isDay)
@@ -36,7 +36,7 @@ namespace StarterPack.Services
 
             if (normalizedCondition.Contains("partly cloudy"))
             {
-                return isDay ? "~/images/weather-icons/Partly cloudy day.png" : "~/images/weather-icons/Partly cloudy night.png";
+                return isDay ? "images/weather-icons/Partly cloudy day.png" : "images/weather-icons/Partly cloudy night.png";
             }
 
             var priorityOrder = new[]
@@ -56,7 +56,7 @@ namespace StarterPack.Services
             }
 
             // Default 
-            return "~/images/weather-icons/Sunny.png";
+            return "images/weather-icons/Sunny.png";
         }
     }
 }
