f19a33aa3ed21c313a4a05bb362327b8405b6d7f
Mihaela Chiaburu <mchiaburu.1337@gmail.com>
2025-09-01 20:14:52 +0300

    fixed

--- StarterPack/Controllers/CalculatorController.cs
+++ StarterPack/Controllers/CalculatorController.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StarterPack.Interfaces;
+using StarterPack.Models.Calculator;
 
 namespace StarterPack.Controllers
 {
     public class CalculatorController : Controller
     {
+        private readonly ICalculatorService _calculatorService;
+
+        public CalculatorController(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult ProcessCalculation([FromBody] CalculationRequest request)
+        {
+            var result = _calculatorService.ProcessCalculation(request);
+            return Json(result);
+        }
+
+        [HttpGet]
+        public JsonResult GetHistory()
+        {
+            return Json(_calculatorService.GetHistory());
+        }
+
+        [HttpPost]
+        public IActionResult ClearHistory()
+        {
+            _calculatorService.ClearHistory();
+            return Ok();
         }
     }
 }
